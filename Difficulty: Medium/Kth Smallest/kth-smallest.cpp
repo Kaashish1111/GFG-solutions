9a86430e4c01ae6201daf3fc7b1dd1a5cefa83d7
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2024-11-04 11:55:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -14,8 +14,19 @@ class Solution {
     // k : find kth smallest element and return using this function
     int kthSmallest(vector<int> &arr, int k) {
         // code here
-        sort(arr.begin(),arr.end());
+        // sort(arr.begin(),arr.end());
         int n=arr.size();
+         if(k<=n){
+            for(int i=0;i<n-1;i++){
+                for(int j=0;j<n-i-1;j++){
+                    if(arr[j]>arr[j+1]){
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+        }
         int ele=arr[k-1];
         return ele;
     }
