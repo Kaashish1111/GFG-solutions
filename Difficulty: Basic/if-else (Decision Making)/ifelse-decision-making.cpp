0ef744cbbb27eee46ddad4d4c439afc1f240a207
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2024-11-22 13:36:32 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Basic/if-else (Decision Making)/ifelse-decision-making.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    string compareNM(int n, int m){
+        // code here
+       return (n<m) ?"lesser": (n>m) ?"greater":"equal";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,m ; cin>>n>>m;
+        Solution obj;
+        cout<<obj.compareNM(n,m)<<"\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
