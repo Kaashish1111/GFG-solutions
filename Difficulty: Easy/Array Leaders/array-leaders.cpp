924d2bb20225a87780b1c87784b6fdd90a66c163
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2025-06-27 16:10:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.cpp
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -1,67 +1,20 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
 
-
-// } Driver Code Ends
 
 class Solution {
-  public:
     // Function to find the leaders in the array.
+  public:
     vector<int> leaders(vector<int>& arr) {
+        // Code here
+        int max=INT_MIN;
+        int n=arr.size();
         vector<int> ans;
-        int maxFromRight = arr[arr.size() - 1];
-        ans.push_back(maxFromRight);  // Last element is always a leader
-
-        // Traverse the array from right to left
-        for (int i = arr.size() - 2; i >= 0; i--) {
-            if (arr[i] >= maxFromRight) {
-                maxFromRight = arr[i];
-                ans.push_back(arr[i]);
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]>=max){
+                max=arr[i];
+                ans.push_back(max);
             }
         }
-        
-        // Reverse the result since we collected leaders from the right
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t; // Number of test cases
-    cin >> t;
-    cin.ignore(); // Ignore the newline after the integer input
-    while (t--) {
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num); // Read the array elements from input string
-
-        Solution obj;
-        vector<int> result = obj.leaders(a);
-
-        // Print the result in the required format
-        if (result.empty()) {
-            cout << "[]"; // Print empty brackets if no leaders are found
-        } else {
-            for (int i = 0; i < result.size(); i++) {
-                if (i != 0)
-                    cout << " ";
-                cout << result[i];
-            }
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
